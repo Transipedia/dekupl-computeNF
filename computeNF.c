a188cb830965a6d6aefe6821ecd7c00b56a656a5
jaudoux <jerome.audoux@gmail.com>
2018-03-12 20:22:44 +0100

    Add logs about iterations

--- computeNF.c
+++ computeNF.c
@@ -157,6 +157,7 @@ int main(int argc, char *argv[])
 
     // We have reached a step, we compute the NF
     if(line % nb_kmers_per_step == 0) {
+      fprintf(stderr, "step %d - Compute normalization_factors\n", nb_step + 1);
       // Create a new array to store the NF
       double * new_nf = compute_nf(norm_counts, nb_samples);
       // We already have done one step, we compare the values
@@ -165,6 +166,7 @@ int main(int argc, char *argv[])
         for(j = 0; j < nb_samples; j++) {
           sum += abs(normalization_factors[j] - new_nf[j]);
         }
+        fprintf(stderr, "Error with previous step : %f\n", sum);
         // New factors replace the old ones
         free(normalization_factors);
         normalization_factors = new_nf;
